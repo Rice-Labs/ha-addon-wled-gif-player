f1d017b2f164e937213ff5b573814f2ee4c918f1
Lucas <lucasrboyer@gmail.com>
2026-05-03 22:28:39 -0400

    Update run.sh

--- wled-gif-streamer/run.sh
+++ wled-gif-streamer/run.sh
@@ -5,15 +5,16 @@ WIDTH=$(bashio::config 'matrix_width')
 HEIGHT=$(bashio::config 'matrix_height')
 MEDIA_DIR="/share/wled_gifs"
 
-bashio::log.info "Starting WLED-video Binary Streamer..."
+bashio::log.info "Starting WLED-video Streamer from subfolder..."
 
 while true; do
     if ls "$MEDIA_DIR"/*.gif >/dev/null 2>&1; then
         for FILE in "$MEDIA_DIR"/*.gif; do
+            [ -e "$FILE" ] || continue
             bashio::log.info "Streaming $(basename "$FILE")"
             
-            # Use the pre-compiled binary
-            ./wledvideo \
+            # Note the path to the script: wled-video/wledvideo.py
+            python3 wled-video/wledvideo.py \
                 --host "$WLED_HOST" \
                 --width "$WIDTH" \
                 --height "$HEIGHT" \
@@ -21,7 +22,7 @@ while true; do
                 "$FILE"
         done
     else
-        bashio::log.warning "No GIFs found in $MEDIA_DIR."
+        bashio::log.warning "No GIFs found in $MEDIA_DIR. Waiting 10 seconds..."
         sleep 10
     fi
 done
